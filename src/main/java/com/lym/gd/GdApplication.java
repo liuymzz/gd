f9b62ab8b8c95ecfaf8b7a7a5239a3ce35346d8f
刘亚明 <liuyaming@liuyamingdeMacBook-Pro.local>
2018-03-11 15:37:35 +0800

    Initial commit

---
+++ src/main/java/com/lym/gd/GdApplication.java
@@ -0,0 +1,16 @@
+package com.lym.gd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuyaming
+ * @date 2018/3/11
+ */
+@SpringBootApplication
+public class GdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdApplication.class, args);
+	}
+}
