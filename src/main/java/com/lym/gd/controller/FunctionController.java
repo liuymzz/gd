abc850c76f7fab8e3326fc07fc0688c4d81f296c
刘亚明 <1424659514@qq.com>
2018-04-09 17:51:50 +0800

    完成课堂互动静态页面、以及FunctionController跟FunctionService

---
+++ src/main/java/com/lym/gd/controller/FunctionController.java
@@ -0,0 +1,19 @@
+package com.lym.gd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author liuyaming
+ * @date 2018/4/9 上午11:04
+ */
+
+@Controller
+public class FunctionController {
+
+    @GetMapping
+    public String interactView(){
+
+        return "";
+    }
+}
---
+++ src/main/java/com/lym/gd/service/FunctionService.java
@@ -0,0 +1,12 @@
+package com.lym.gd.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author liuyaming
+ * @date 2018/4/9 上午11:04
+ */
+
+@Service
+public class FunctionService {
+}
