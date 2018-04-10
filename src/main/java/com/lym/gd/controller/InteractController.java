01e54026960313659eadc5c4a36c9a71d160ddcc
刘亚明 <1424659514@qq.com>
2018-04-10 15:35:11 +0800

    为了直观性，去掉FunctionController跟FunctionService，Controller跟Service的名称根据功能来定义

--- src/main/java/com/lym/gd/controller/InteractController.java
+++ src/main/java/com/lym/gd/controller/InteractController.java
@@ -4,17 +4,14 @@ import com.lym.gd.service.FunctionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author liuyaming
- * @date 2018/4/9 上午11:04
+ * @date 2018/4/10 下午3:32
  */
-
 @Controller
-public class FunctionController {
-
+public class InteractController {
     @Autowired
     private FunctionService functionService;
 
@@ -28,4 +25,5 @@ public class FunctionController {
     public String getSessionId(){
         return functionService.sessionId();
     }
+
 }
--- src/main/java/com/lym/gd/service/InteractService.java
+++ src/main/java/com/lym/gd/service/InteractService.java
@@ -7,17 +7,17 @@ import javax.servlet.http.HttpSession;
 
 /**
  * @author liuyaming
- * @date 2018/4/9 上午11:04
+ * @date 2018/4/10 下午3:32
  */
-
 @Service
-public class FunctionService {
+public class InteractService {
     @Autowired
     private HttpSession httpSession;
 
     public String sessionId(){
         return httpSession.getId();
     }
+
 
 
 }
