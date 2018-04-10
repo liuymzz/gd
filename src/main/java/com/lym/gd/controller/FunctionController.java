60016e50bfcd71bd947dbfcecefd847aa235e4b3
刘亚明 <1424659514@qq.com>
2018-04-10 13:20:51 +0800

    添加Redis依赖 '/interact'添加name属性

--- src/main/java/com/lym/gd/controller/FunctionController.java
+++ src/main/java/com/lym/gd/controller/FunctionController.java
@@ -12,8 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FunctionController {
 
     @GetMapping("/interact")
-    public String interactView(){
-
+    public String interactView(String name){
         return "other/interact";
     }
 }
