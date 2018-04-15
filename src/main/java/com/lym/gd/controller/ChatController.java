241209947d9486671dfd5f35407c3ac248f4f849
刘亚明 <1424659514@qq.com>
2018-04-15 09:46:05 +0800

    当URL中h参数的值在Redis中不存在时，说明不存在对应的课堂，那么需要返回到404页面

--- src/main/java/com/lym/gd/controller/ChatController.java
+++ src/main/java/com/lym/gd/controller/ChatController.java
@@ -1,10 +1,14 @@
 package com.lym.gd.controller;
 
+import com.lym.gd.service.RedisService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Set;
 
 /**
  * @author liuyaming
@@ -12,14 +16,23 @@ import javax.servlet.http.HttpServletRequest;
  */
 
 @Controller
+@Slf4j
 public class ChatController {
+
+    @Autowired
+    private RedisService redisService;
 
     @GetMapping("/chat")
     public String chatView(Model model, String h, HttpServletRequest request){
 
         String hh = request.getParameter("h");
 
-        model.addAttribute("channel",h);
+        Set<String> keys = redisService.getKeysByPrefix(h);
+
+        if (keys.size() > 0) {
+
+            model.addAttribute("channel", h);
+        }
 
         return "other/chat";
     }
